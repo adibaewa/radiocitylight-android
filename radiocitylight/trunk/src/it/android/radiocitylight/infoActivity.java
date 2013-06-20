0eaad01e8f8b96278485ee5f34515fd97aca8be4
senzacquacarlo@gmail.com <senzacquacarlo@gmail.com@50e64262-9203-a2ef-c47c-e4881644ec70>
2013-06-20 09:55:31 +0000

    Add info

---
+++ radiocitylight/trunk/src/it/android/radiocitylight/infoActivity.java
@@ -0,0 +1,14 @@
+package it.android.radiocitylight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by carlo on 20/06/13.
+ */
+public class infoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
